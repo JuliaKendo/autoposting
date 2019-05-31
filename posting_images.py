cb16b2d3792e55ba38a5de20080af312ecb8a63b
JuliaKendo <50887767+JuliaKendo@users.noreply.github.com>
2019-06-01 00:32:18 +0300

    Update posting_images.py

--- posting_images.py
+++ posting_images.py
@@ -1,15 +1,16 @@
 import os
 import time
+import logging
 from dotenv import load_dotenv
 from instabot import Bot
 
-timeout = 60
-included_extensions = ['jpg','jpeg', 'tif', 'png', 'gif']
-proxy = None
+TIMEOUT = 60
+INCLUDED_EXTENSIONS = ['jpg','jpeg', 'tif', 'png', 'gif']
+PROXY = None
 
-def get_image_dir():
+def get_image_directory():
     current_directory = os.path.dirname(__file__)
-    return current_directory+'\\images'
+    return os.path.join(current_directory,'images')
 
 def posting_images():
 
@@ -17,23 +18,23 @@ def posting_images():
     inta_user = os.getenv("instauser")
     insta_password = os.getenv("instapassword")
 
-    image_directory = get_image_dir()
+    image_directory = get_image_directory()
     file_names = os.listdir(image_directory)
       
     upload_images = [fn for fn in file_names
-              if any(fn.endswith(ext) for ext in included_extensions)] 
+              if any(fn.endswith(ext) for ext in INCLUDED_EXTENSIONS)] 
 
     bot = Bot()
-    bot.login(username=inta_user, password=insta_password, proxy=proxy)
+    bot.login(username=inta_user, password=insta_password, proxy=PROXY)
 
     for upload_image in upload_images:
         if upload_image:
-            file_path = f'{image_directory}\\{upload_image}'
+            file_path = os.path.join(image_directory,upload_image)
             try:
                 bot.upload_photo(file_path, caption=upload_image)
                 if bot.api.last_response.status_code != 200:
-                    print(bot.api.last_response)
+                    logging.error(bot.api.last_response)
             except Exception as error:
-                print(str(error))
+                logging.error("Ошибка загрузки фото в инстаграмм: {}".format(error))
             
-            time.sleep(timeout)
+            time.sleep(TIMEOUT)
