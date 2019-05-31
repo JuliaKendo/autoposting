d5e2cb33d56ef932f2ace23f1d50c7cb6335f3a0
JuliaKendo <50887767+JuliaKendo@users.noreply.github.com>
2019-06-01 00:31:51 +0300

    Update fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,19 +1,18 @@
+import logging
 import requests
 import os
 
-filename_template = 'spacex{0}.jpeg'
-url_template = 'https://api.spacexdata.com/v3/launches/latest'
+FILENAME_TEMPLATE = 'spacex{0}.jpeg'
+URL_TEMPLATE = 'https://api.spacexdata.com/v3/launches/latest'
 
-def get_image_dir():
+def get_image_directory():
     current_directory = os.path.dirname(__file__)
-    return current_directory+'\\images'
+    return os.path.join(current_directory,'images')
 
 def save_image(url, filename):
-    image_directory = get_image_dir()
-    file_path = f'{image_directory}\\{filename}'
-
-    if not os.path.exists(image_directory):
-        os.mkdir(image_directory)
+    image_directory = get_image_directory()
+    file_path = os.path.join(image_directory,filename)
+    os.makedirs(image_directory, exist_ok=True) 
 
     responce = requests.get(url)
     responce.raise_for_status()
@@ -25,14 +24,14 @@ def save_image(url, filename):
 
 def fetch_spacex_last_launch():
     
-    responce = requests.get(url_template)
+    responce = requests.get(URL_TEMPLATE)
     responce.raise_for_status()
 
     responce_json = responce.json()
     images_list = responce_json['links']['flickr_images']
     for num, image_url in enumerate(images_list):
         if image_url:
-            image_name = filename_template.format(num)
-            print(f'Загружаеться картинка: {image_name}')
+            image_name = FILENAME_TEMPLATE.format(num)
+            logging.info(f'Загружаеться картинка: {image_name}')
             saved_filename = save_image(image_url, image_name)
-            print(f'Загружена картинка: {saved_filename}')
+            logging.info(f'Загружена картинка: {saved_filename}')
